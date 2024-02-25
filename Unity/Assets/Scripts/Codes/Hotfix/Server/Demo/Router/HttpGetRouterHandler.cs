518471d3d66fe74f53da255594ea761a6b9b7fc2
Jss <275442646@qq.com>
2024-02-25 13:38:02 +0800

    加上注释，这块容易错误

--- Unity/Assets/Scripts/Codes/Hotfix/Server/Demo/Router/HttpGetRouterHandler.cs
+++ Unity/Assets/Scripts/Codes/Hotfix/Server/Demo/Router/HttpGetRouterHandler.cs
@@ -6,19 +6,22 @@ using System.Text;
 namespace ET.Server
 {
     [HttpHandler(SceneType.RouterManager, "/get_router")]
-    public class HttpGetRouterHandler : IHttpHandler
+    public class HttpGetRouterHandler: IHttpHandler
     {
         public async ETTask Handle(Scene scene, HttpListenerContext context)
         {
             HttpGetRouterResponse response = new();
             foreach (StartSceneConfig startSceneConfig in StartSceneConfigCategory.Instance.Realms)
             {
+                // 这里是要用InnerIP，因为云服务器上realm绑定不了OuterIP的,所以realm的内网外网的socket都是监听内网地址
                 response.Realms.Add(startSceneConfig.InnerIPPort.ToString());
             }
+
             foreach (StartSceneConfig startSceneConfig in StartSceneConfigCategory.Instance.Routers)
             {
                 response.Routers.Add($"{startSceneConfig.StartProcessConfig.OuterIP}:{startSceneConfig.Port}");
             }
+
             HttpHelper.Response(context, response);
             await ETTask.CompletedTask;
         }
